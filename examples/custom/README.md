3bd1391478f284bc1478eef486d2b64f8953b38e
Gregorio Galante <gregogalante@gmail.com>
2017-12-22 11:48:20 +0100

    completed custom application example

---
+++ examples/custom/README.md
@@ -0,0 +1,13 @@
+# Custom application example
+
+Example of Evnt gem usage for a custom application.
+
+## Usage
+
+```shell
+
+bundle install
+
+ruby custom.rb
+
+```
--- examples/custom/custom.rb
+++ examples/custom/custom.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# This is an example of application that use evnt gem to work.
-
 require_relative 'commands/create_text_file_command'
 
 command = CreateTextFileCommand.new(name: 'foo')
