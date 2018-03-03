48cad38a36f3131401bd6f18dbb6145339bd4dfa
Gregorio Galante <gregogalante@gmail.com>
2018-03-03 10:54:48 +0100

    added default options on initializer generator for rails

--- lib/generators/evnt/templates/initializer/app/events/application_event.rb
+++ lib/generators/evnt/templates/initializer/app/events/application_event.rb
@@ -2,4 +2,14 @@
 
 # ApplicationEvent.
 class ApplicationEvent < Evnt::Event
+
+  default_options silent: false
+
+  to_write_event do
+    # EventModel.create(
+    #   name: name,
+    #   payload: payload
+    # )
+  end
+
 end
